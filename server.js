72d3f56b278d6890caef04046cd20cf6946f2a55
AgileKuraKani <agilekurakani@gmail.com>
2020-12-23 12:41:45 +0545

    Basic Insatllation of packages complete

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 const http = require('http');
 const container = require('./container');
 
-container.resolve(function(){
+container.resolve(function(users){
     const app = SetupExpress();
     function SetupExpress(){
         const app = express();
@@ -12,5 +12,22 @@ container.resolve(function(){
         server.listen(3000, function(){
             console.log('Listening to port No 3000');
         });
+
+    ConfigureExpress(App);
+
+          //Router Setup
+    const router = require('express-promise-router')();
+    users.SetRouting(router);
+    app.use(router);
     }
+  
+
+
+    function Configuration(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyparser.json());
+        app.use(bodayParser.urlencoded({extended: true}));
+    }    
+
 });
