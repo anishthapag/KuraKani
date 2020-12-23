daef459f99f98ade215fe72ec3ff8b4b547a99a5
AgileKuraKani <agilekurakani@gmail.com>
2020-12-23 12:09:16 +0545

    Creating Server

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
 const container = require('./container');
+
+container.resolve(function(){
+    const app = SetupExpress();
+    function SetupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, function(){
+            console.log('Listening to port No 3000');
+        });
+    }
+});
